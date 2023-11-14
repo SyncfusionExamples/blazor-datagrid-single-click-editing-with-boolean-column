475badb6030e5d9b941719c3eab56b20eebe16b2
Backia Raj Kanagaraj <backiaraj.kanagaraj@syncfusion.com>
2023-11-14 11:51:43 +0530

    Update the project

--- Data/WeatherForecast.cs
+++ Data/WeatherForecast.cs
@@ -1,8 +1,8 @@
-namespace Editing_BooleanColumn.Data
+namespace SingleClickEditing.Data
 {
     public class WeatherForecast
     {
-        public DateTime Date { get; set; }
+        public DateOnly Date { get; set; }
 
         public int TemperatureC { get; set; }
 
